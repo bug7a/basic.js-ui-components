cd42aa6a5a3183135631e259039e45bd98590b72
Buğra ÖZDEN <bugra.ozden@gmail.com>
2024-07-12 21:21:02 +0300

    Update

--- components/_component-template.js
+++ components/_component-template.js
@@ -16,33 +16,34 @@ Web: https://bug7a.github.io/basic.js-ui-components/
 */
 
 "use strict";
+
+// Default values
+const CompNameDefaults = {
+    key: 0,
+    border: 1, // Standard box features are added automatically.
+    width: 90,
+    height: 50,
+    round: 4,
+    onClick: function(self) {},
+    boxColor: "white", // I didn't use color because I didn't want to add it automatically.
+    boxOverColor: "#8FC7B9",
+    iconFile: "assets/close.svg",
+    iconColor: "transparent",
+    iconBorder: 0,
+    badgetText: "",
+};
+
 const CompName = function(params = {}) {
-
-    // Default values
-    const defaults = {
-        border: 1, // Standard box features are added automatically.
-        width: 90,
-        height: 50,
-        round: 4,
-        box_backgroundColor: "white", // I didn't use color because I didn't want to add it automatically.
-        box_backgroundOverColor: "#8FC7B9",
-        icon_file: "assets/close.svg",
-        icon_backgroundColor: "transparent",
-        icon_border: 0,
-        badget_text: "",
-    };
-
-    if (params == "get") return defaults; // CompName("get").border
 
     // BOX: Component container
     const box = startObject();
 
     // After box created; you can take values from containerBox
-    //defaults.round = box.containerBox.round;
-    //defaults.color = box.containerBox.color;
+    //CompNameDefaults.round = box.containerBox.round;
+    //CompNameDefaults.color = box.containerBox.color;
 
     // Values are ready to use
-    box.props(defaults, params);
+    box.props(CompNameDefaults, params);
 
     // *** PRIVATE VARIABLES:
     //let privateVar = "";
@@ -51,7 +52,7 @@ const CompName = function(params = {}) {
         { min: 4, max: 10, color: "gold" },
         { min: 11, max: 100, color: "#D96450" },
     ];
-    let badget_backgroundColor = null;
+    let badgetCurrentColor = null;
 
     // *** PUBLIC VARIABLES:
     //box.publicVar = "";
@@ -65,15 +66,15 @@ const CompName = function(params = {}) {
     // If you need to change a param after it is created. You can write a setter function for it.
     //box.publicFunc = function() {};
 
-    box.setIcon_file = function(iconFile) {
+    box.setIconFile = function(iconFile) {
         box.iconFile = iconFile;
         box.icoLogo.load(iconFile);
     }
-    // USAGE: get: componentName.icon_file, set: componentName.setIcon_file("8")
+    // USAGE: get: componentName.iconFile, set: componentName.setIconFile("8")
 
-    box.setBadget_text = function(text) {
+    box.setBadgetText = function(text) {
 
-        box.badget_text = text;
+        box.badgetText = text;
         box.lblBadget.text = text;
 
         const _num = parseInt(text);
@@ -81,20 +82,20 @@ const CompName = function(params = {}) {
         badgetColors.forEach(function(data, index) {
             if (_num >= data.min && _num <= data.max) {
                 box.lblBadget.color = data.color;
-                badget_backgroundColor = data.color;
+                badgetCurrentColor = data.color;
             }
         });
 
     };
-    // USAGE: get: componentName.badget_text, set: componentName.setBadget_text("8")
+    // USAGE: get: componentName.badgetText, set: componentName.setBadgetText("8")
 
-    box.setBox_backgroundColor = function(color) {
-        box.box_backgroundColor = color;
+    box.setBoxColor = function(color) {
+        box.boxColor = color;
         box.color = color;
     };
 
-    box.setBox_backgroundOverColor = function(color) {
-        box.box_backgroundOverColor = color;
+    box.setBoxOverColor = function(color) {
+        box.boxOverColor = color;
     };
 
     // *** OBJECT VIEW:
@@ -102,7 +103,7 @@ const CompName = function(params = {}) {
     box.clickable = 1;
     // Show outside of the box.
     box.elem.style.overflow = "visible";
-    box.color = box.box_backgroundColor;
+    box.color = box.boxColor;
     //box.borderColor = "indianred";
     box.setMotion("background-color 0.3s");
     
@@ -118,15 +119,15 @@ const CompName = function(params = {}) {
             height: 32,
             space: 10,
             opacity: 0.7,
-            border: box.icon_border,
-            color: box.icon_backgroundColor,
+            border: box.iconBorder,
+            color: box.iconColor,
         });
-        that.load(box.icon_file);
+        that.load(box.iconFile);
         that.center();
 
         // LABEL: Badget
         box.lblBadget = Label({
-            text: "", // If you have complex functions just use box.setBadget_text(box.badget_text); after OBJECT INIT CODE.
+            text: "", // If you have complex functions just use box.setBadgetText(box.badgetText); after OBJECT INIT CODE.
             textColor: "rgba(0, 0, 0, 0.8)",
             border: 1,
             borderColor: "rgba(0, 0, 0, 0.6)",
@@ -142,15 +143,18 @@ const CompName = function(params = {}) {
         that.setMotion("background-color 0.3s");
 
     // *** OBJECT INIT CODE:
+    box.elem.addEventListener("click", function() {
+        box.onClick(box);
+    });
     box.elem.addEventListener("mouseover", function() {
-        box.color = box.box_backgroundOverColor;
+        box.color = box.boxOverColor;
         box.lblBadget.color = "white";
     });
     box.elem.addEventListener("mouseout", function() {
-        box.color = box.box_backgroundColor;
-        box.lblBadget.color = badget_backgroundColor;
+        box.color = box.boxColor;
+        box.lblBadget.color = badgetCurrentColor;
     });
-    box.setBadget_text(box.badget_text);
+    box.setBadgetText(box.badgetText);
     
     return endObject(box);
 
